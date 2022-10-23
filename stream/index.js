05215c9217d184636b589cfec09071953b2ef2da
owaissyed65 <owaissyed65@gmail.com>
2022-10-23 21:47:13 +0500

    adding basics

--- stream/index.js
+++ stream/index.js
@@ -4,8 +4,33 @@ const http = require('http').createServer();
 // rhttp.on('connect',()=>{
 //     console.log('spme')
 // })
-const stream = fs.createReadStream('hello.txt', 'utf-8');
+const stream = fs.createReadStream('hello.txt');
 
-http.on('request',()=>{
-    
+http.on('request', (req, res) => {
+    console.log('done')
+    // 1st way
+    // stream.on('data', (chunkData) => {
+    //     res.write(chunkData)
+    // });
+    // stream.on('end', () => {
+    //     res.end()
+    // });
+    // stream{
+    //     data,end,error,close
+    // }
+
+
+    // 2nd way
+    // fs.readFile('hello.txt', 'utf-8', (err, data) => {
+    //     res.end(data);
+    // })
+
+
+    // 3rd way
+    stream.pipe(res, false)
+
+
+});
+http.listen(5000, '127.0.0.1', () => {
+    console.log(`Ports listening on ${5000} in http://localhost:5000`)
 })
